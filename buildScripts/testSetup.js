e301de71d58056fe57e9e111b114ec25631a5f37
Daniel Cantwell <dcantwel@usc.edu>
2017-07-12 17:23:48 -0700

    Init commit, work in progress.  Adding TravisCI config

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,6 @@
+// Not transpiled, so must use ES5
+
+// Register babel to transpile before our tests run
+require('babel-register')();
+// Disable webpack features that Mocha doesn't understand
+require.extensions['.css'] = function() {};
